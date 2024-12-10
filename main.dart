bb2a9262ea0657336eefb7f42d7e3ac35721e2ff
Yansuputod <arta70660@gmail.com>
2024-12-10 09:32:03 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(){
+  int input = 7;
+  String text = "";
+  for(int i = 0; i < input; i++){
+    for(int j = 0; j < input; j++){
+      text = "$text "+ "*";
+    }
+    print(text);
+    text = "";
+  }
+}
